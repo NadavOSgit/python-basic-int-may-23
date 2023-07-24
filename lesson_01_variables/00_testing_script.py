cf9840db9eba9128a15869f7271f1291915d90cb
NadavOSgit <nadavpe98@gmail.com>
2023-07-24 20:41:27 +0300

    Class Done until input

--- lesson_01_variables/00_testing_script.py
+++ lesson_01_variables/00_testing_script.py
@@ -1,4 +1,4 @@
-a = 10
+a = 10   #its variable 10
 print(a)
 print(type(a))
 a = "Alex"
@@ -8,3 +8,15 @@ print(type(a))
 a =10.3
 print(a)
 print(type(a))
+
+
+my_list = [1, "nadav", 2, "bla bla"]
+print(my_list)
+print(type(my_list))
+
+print (type("nadav"))
+
+my_name = '''
+print alex
+''' #this is NOT a comment!
+
---
+++ lesson_01_variables/03_numbers.py
@@ -0,0 +1,21 @@
+int_1 = 56
+int_2 = 2324515643
+int_3 = 46354
+
+float_1 = 1.45
+float_2 = -1.45
+float_3 = 35e3
+
+print(float_3)
+
+complex_1 = 5j
+print(complex_1)
+
+num_1 = 10000
+num_2 = 10000
+
+print(id(num_1))
+print(id(num_2))
+
+
+
