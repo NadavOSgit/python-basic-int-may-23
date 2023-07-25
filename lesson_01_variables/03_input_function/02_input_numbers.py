65a2fd262a6513b2cc5c091f536d2ec15d25cf5e
NadavOSgit <nadavpe98@gmail.com>
2023-07-25 15:16:03 +0300

    some corrections

--- lesson_01_variables/03_input_function/02_input_numbers.py
+++ lesson_01_variables/03_input_function/02_input_numbers.py
@@ -2,3 +2,4 @@ number_1 = input("Please enter 1st number: ")
 number_2 = input("Please enter 2nd number: ")
 print(type(number_2))
 print(f"{number_1} + {number_2} = {number_1 + number_2}")
+                                          
