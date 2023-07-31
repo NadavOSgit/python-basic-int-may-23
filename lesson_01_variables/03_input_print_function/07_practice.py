af135c0f0534e279ab747139618353db4a3cdc91
NadavOSgit <nadavpe98@gmail.com>
2023-07-31 20:35:04 +0300

    finish lesson save

---
+++ lesson_01_variables/03_input_print_function/07_practice.py
@@ -0,0 +1,47 @@
+#  solution 1 #
+# num = int(input("Enter you number here: "))
+#
+# print(num * 2)
+# print(num / 2)
+#
+# solution 2 #
+# sqer = float(input("enter the lenth on side a: "))
+#
+# print(sqer ** 2)
+#
+# sqer = float(input("enter the lenth on side a: "))
+#
+# print(pow(sqer ,2))
+#
+# solution 3 #
+# number_1, number_2 = map(int, input("puth the score here: ").split())
+#
+# print(number_1 + number_2)
+#
+# solution 4 #
+#
+# fahrenheight = float(input())
+#
+# print((fahrenheight - 32) * 5 / 9)
+#
+# n1 ,n2 ,n3 ,n4 = map(int, input("enter 4 numbers here: ").split())
+#
+# print((n1+n2+n3+n4)/4)
+#
+# OR
+#
+# list_of_nums = list(map(int, input("put the score here: ").split()))
+#
+# print(sum(list_of_nums) / len(list_of_nums))
+#
+# solution 5 #
+# a, b, c, d, e = map(int, input("Please enter 5 grades: ").split())
+# print(max(a, b, c, d, e))
+#
+#
+# print(max([15, 58, 100, 89, 100]))
+#
+# solution 6 #
+
+the_num = int(input("enter the number here: "))
+print(the_num-1, the_num, the_num+1, sep='<')
