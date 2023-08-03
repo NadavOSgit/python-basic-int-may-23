db40ba369a6d9fe41d350648bdcc68b0aab79214
NadavOSgit <nadavpe98@gmail.com>
2023-08-03 20:35:01 +0300

    Lesson python 3

---
+++ lesson_01_variables/05_booleans/03_logical_operators.py
@@ -0,0 +1,11 @@
+# And
+print(f"True and True = {True and True}")
+print(f"4 > 3 and 5 > 4 = {4 > 3 and 5 > 4}")
+
+print(f"False and True = {False and True}")
+print(f"4 < 3 and 5 > 4 = {4 > 3 and 5 > 4}")
+
+print(f"False and False = {False and False}")
+print(f"True and True = {True and True}")
+
+
