469ef9d0e0a89decffb661070033c28c36ac1b18
NadavOSgit <nadavpe98@gmail.com>
2024-01-04 18:27:27 +0200

    retry

--- part_16_telegram_bots/03_differemt_types_hendler.py
+++ part_16_telegram_bots/03_differemt_types_hendler.py
@@ -11,18 +11,12 @@ dp = Dispatcher()
 
 @dp.message()
 async def answer_as_echo(message: types.Message):
-    await message.answer(text=message.text)
-
+    if message.text:
+        await message.answer(text=message.text)
+    elif message.sticker:
+        await message.answer("sticker was used")
+        await message.reply_sticker(sticker=message.sticker.file_id)
 
 @dp.message()
 async def reply_as_echo(message: types.Message):
     await message.reply(text=message.text)
-
-
-async def main():
-    logging.basicConfig(level=logging.INFO)
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
